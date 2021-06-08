f9ba8b29c05173aad9f869058d9439af608254e4
th817 <73452842+th817@users.noreply.github.com>
2021-06-08 16:50:59 +0100

    Added U_SU4 Ansatz

--- Final QOSF_project/unitary.py
+++ Final QOSF_project/unitary.py
@@ -78,3 +78,17 @@ def U_SO4(params, wires):  # 6 params
     qml.CNOT(wires=[wires[0], wires[1]])
     qml.RY(params[4], wires=wires[0])
     qml.RY(params[5], wires=wires[1])
+
+
+def U_SU4(params, wires): # 15 params
+    qml.U3(params[0], params[1], params[2], wires=wires[0])
+    qml.U3(params[3], params[4], params[5], wires=wires[1])
+    qml.CNOT(wires=[wires[0], wires[1]])
+    qml.RY(params[6], wires=wires[0])
+    qml.RZ(params[7], wires=wires[1])
+    qml.CNOT(wires=[wires[1], wires[0]])
+    qml.RY(params[8], wires=wires[0])
+    qml.CNOT(wires=[wires[0], wires[1]])
+    qml.U3(params[9], params[10], params[11], wires=wires[0])
+    qml.U3(params[12], params[13], params[14], wires=wires[1])
+
