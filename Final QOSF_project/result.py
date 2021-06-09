0d5b6c4523d14e45d79535a0fb870cd0f547f141
th817 <73452842+th817@users.noreply.github.com>
2021-06-09 02:35:02 +0100

    created a module generating the results of the benchmark

---
+++ Final QOSF_project/result.py
@@ -0,0 +1,11 @@
+# This generates the results of the bechmarking code
+
+import Benchmarking
+
+U = 'U_TTN'
+U_params = 2
+dataset = 'MNIST'
+Encoding = 'resize256'
+circuit = 'QCNN'
+
+Benchmarking.Benchmarking(dataset, U, U_params, Encoding, circuit)
