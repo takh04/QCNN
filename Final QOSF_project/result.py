32264fb239b71842923e0d1fee4c9f8079f066ea
th817 <73452842+th817@users.noreply.github.com>
2021-06-10 14:25:19 +0100

    Results for hybrid Embedding

--- Final QOSF_project/result.py
+++ Final QOSF_project/result.py
@@ -2,10 +2,13 @@
 
 import Benchmarking
 
+#########
+# Here are possible combinations of benchmarking user could try.
 # Unitaries = [U_TTN, U_5, U_6, U_9, U_13, U_14, U_15, U_SO4, U_SU4]
 # U_num_params = [2, 10, 10, 2, 6, 6, 4, 6, 15]
 # dataset = 'mnist' , 'fashion_mnist'
 # circuit = 'QCNN', 'Hierarchical'
+#########
 
 Unitaries = ['U_SU4']
 U_num_params = [15]
