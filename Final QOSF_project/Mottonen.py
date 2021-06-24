5345ae99d90a60350ffa51de5c85afabf90a09b3
th817 <73452842+th817@users.noreply.github.com>
2021-06-25 00:05:31 +0100

    changed name to angular mottonen

--- Final QOSF_project/Mottonen.py
+++ Final QOSF_project/Mottonen.py
@@ -2,13 +2,13 @@
 import pennylane as qml
 
 # For Hybrid 16 / 4 bits of information is embedded in 2 wires
-def Mottonen_16(X, wires):
+def Angular_Mottonen_16(X, wires):
     # This is not an accurate code / must be modified
     qml.RY(X[0], wires = wires[0])
     qml.CRY(X[1], wires = wires[0,1])
 
 # For Hybrid 32 / 16 bits of information is embedded in 4 wires
-def Mottonen_32(X, wires):
+def Angular_Mottonen_32(X, wires):
     # This is not an accurate code / must be modified
     qml.RY(X[0], wires = wires[0])
     qml.CRY(X[1], wires = wires[0,1])
