d85867cc8fba5441e561b800c058a949bcdcfb01
th817 <73452842+th817@users.noreply.github.com>
2021-07-26 18:14:02 +0900

    removed training function

--- Final QOSF_project/CNN.py
+++ Final QOSF_project/CNN.py
@@ -27,63 +27,57 @@ def accuracy_test(predictions, labels):
     acc = acc / len(labels)
     return acc
 
-def CNN_training(X_train, X_test, Y_train, Y_test, input_size, optimizer, steps, n_feature, batch_size):
-    final_layer_size = int(input_size / 4)
-    CNN = nn.Sequential(
-        nn.Conv1d(in_channels=1, out_channels=n_feature, kernel_size=2, padding=1),
-        nn.ReLU(),
-        nn.MaxPool1d(kernel_size=2),
-        nn.Conv1d(in_channels=n_feature, out_channels=n_feature, kernel_size=2, padding=1),
-        nn.ReLU(),
-        nn.MaxPool1d(kernel_size=2),
-        nn.Flatten(),
-        nn.Linear(n_feature * final_layer_size, 2)
-    )
-
-    loss_history = []
-    for it in range(steps):
-        batch_idx = np.random.randint(0, len(X_train), batch_size)
-        X_train_batch = np.array([X_train[i] for i in batch_idx])
-        Y_train_batch = np.array([Y_train[i] for i in batch_idx])
-
-        X_train_batch_torch = torch.tensor(X_train_batch, dtype=torch.float32)
-        X_train_batch_torch.resize_(batch_size, 1, input_size)
-        Y_train_batch_torch = torch.tensor(Y_train_batch, dtype=torch.long)
-
-        criterion = nn.CrossEntropyLoss()
-        if optimizer == 'adam':
-            opt = torch.optim.Adam(CNN.parameters(), lr=0.01, betas=(0.9, 0.999))
-        elif optimizer == 'nesterov':
-            opt = torch.optim.SGD(CNN.parameters(), lr=0.01, momentum=0.9, nesterov=True)
-
-        Y_pred_batch_torch = CNN(X_train_batch_torch)
-
-        loss = criterion(Y_pred_batch_torch, Y_train_batch_torch)
-        loss_history.append(loss.item())
-        if it % 10 == 0:
-            print("[iteration]: %i, [LOSS]: %.6f" % (it, loss.item()))
-
-        opt.zero_grad()
-        loss.backward()
-        opt.step()
-
-        X_test_torch = torch.tensor(X_test, dtype=torch.float32)
-        X_test_torch.resize_(len(X_test), 1, input_size)
-        Y_pred = CNN(X_test_torch).detach().numpy()
-        accuracy = accuracy_test(Y_pred, Y_test)
-        N_params = get_n_params(CNN)
-
-    return loss_history, accuracy, N_params
-
-
-
 def Benchmarking_CNN(dataset, classes, Encodings, Encodings_size, binary, optimizer, steps, n_feature, batch_size):
     for i in range(len(Encodings)):
         Encoding = Encodings[i]
         input_size = Encodings_size[i]
+        final_layer_size = int(input_size / 4)
+        X_train, X_test, Y_train, Y_test = data.data_load_and_process(dataset, classes=[0, 1], feature_reduction=Encoding, binary=False)
 
-        X_train, X_test, Y_train, Y_test = data.data_load_and_process(dataset, classes, feature_reduction = Encoding, binary =binary)
-        loss_history, accuracy, N_params = CNN_training(X_train, X_test, Y_train, Y_test, input_size, optimizer, steps, n_feature, batch_size)
+        CNN = nn.Sequential(
+            nn.Conv1d(in_channels=1, out_channels=n_feature, kernel_size=2, padding=1),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=2),
+            nn.Conv1d(in_channels=n_feature, out_channels=n_feature, kernel_size=2, padding=1),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=2),
+            nn.Flatten(),
+            nn.Linear(n_feature * final_layer_size, 2)
+        )
+
+        loss_history = []
+        for it in range(steps):
+            batch_idx = np.random.randint(0, len(X_train), batch_size)
+            X_train_batch = np.array([X_train[i] for i in batch_idx])
+            Y_train_batch = np.array([Y_train[i] for i in batch_idx])
+
+            X_train_batch_torch = torch.tensor(X_train_batch, dtype=torch.float32)
+            X_train_batch_torch.resize_(batch_size, 1, input_size)
+            Y_train_batch_torch = torch.tensor(Y_train_batch, dtype=torch.long)
+
+            criterion = nn.CrossEntropyLoss()
+            if optimizer == 'adam':
+                opt = torch.optim.Adam(CNN.parameters(), lr=0.01, betas=(0.9, 0.999))
+            elif optimizer == 'nesterov':
+                opt = torch.optim.SGD(CNN.parameters(), lr=0.01, momentum=0.9, nesterov=True)
+
+            Y_pred_batch_torch = CNN(X_train_batch_torch)
+
+            loss = criterion(Y_pred_batch_torch, Y_train_batch_torch)
+            loss_history.append(loss.item())
+            if it % 10 == 0:
+                print("[iteration]: %i, [LOSS]: %.6f" % (it, loss.item()))
+
+            opt.zero_grad()
+            loss.backward()
+            opt.step()
+
+            X_test_torch = torch.tensor(X_test, dtype=torch.float32)
+            X_test_torch.resize_(len(X_test), 1, input_size)
+            Y_pred = CNN(X_test_torch).detach().numpy()
+            accuracy = accuracy_test(Y_pred, Y_test)
+            N_params = get_n_params(CNN)
+
 
         f = open('Result/result_CNN.txt', 'a')
         f.write("Loss History for CNN with " + str(Encoding) + ":" )
@@ -108,5 +102,5 @@ Encodings_size = [256, 8, 8, 16, 16]
 for i in range(5):
     Benchmarking_CNN(dataset=dataset, classes=classes, Encodings=Encodings, Encodings_size=Encodings_size,
                      binary=binary, optimizer='adam', steps=steps, n_feature=3, batch_size=25)
-    Benchmarking_CNN(dataset=dataset, classes=classes, Encodings=Encodings, Encodings_size=Encodings_size,
-                     binary=binary, optimizer='nesterov', steps=steps, n_feature=3, batch_size=25)
+    #Benchmarking_CNN(dataset=dataset, classes=classes, Encodings=Encodings, Encodings_size=Encodings_size,
+    #                 binary=binary, optimizer='nesterov', steps=steps, n_feature=3, batch_size=25)
