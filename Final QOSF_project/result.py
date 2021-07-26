21b238fea1319666178223dca2329191c2e978d8
th817 <73452842+th817@users.noreply.github.com>
2021-07-26 14:04:52 +0900

    result for hybrid embeddings with all unitaries

--- Final QOSF_project/result.py
+++ Final QOSF_project/result.py
@@ -13,7 +13,7 @@ import Benchmarking
 
 Unitaries = ['U_TTN', 'U_5', 'U_6', 'U_9', 'U_13', 'U_14', 'U_15', 'U_SO4', 'U_SU4', 'U_SU4_without_pooling']
 U_num_params = [2, 10, 10, 2, 6, 6, 4, 6, 15, 15]
-Encodings = ['resize256', 'autoencoder8', 'pca8', 'pca16-compact', 'autoencoder16-compact']
+Encodings = ['pca30-3', 'autoencoder30-3', 'pca32-3', 'autoencoder32-3']
 dataset = 'mnist'
 circuit = 'QCNN'
 classes = [0,1]
