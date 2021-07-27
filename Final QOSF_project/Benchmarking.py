8aea7834c5e82e86771c3436efbaacef5a888f9e
th817 <73452842+th817@users.noreply.github.com>
2021-07-27 18:28:41 +0900

    added benchmarking and accuracy test for CrossEntropyLoss

--- Final QOSF_project/Benchmarking.py
+++ Final QOSF_project/Benchmarking.py
@@ -4,20 +4,33 @@ import QCNN_circuit
 import Hierarchical_circuit
 import numpy as np
 
-def accuracy_test(predictions, labels, binary = True):
-    if binary == True:
+def accuracy_test(predictions, labels, cost_fn, binary = True):
+    if cost_fn == 'mse':
+        if binary == True:
+            acc = 0
+            for l, p in zip(labels, predictions):
+                if np.abs(l - p) < 1:
+                    acc = acc + 1
+            return acc / len(labels)
+
+        else:
+            acc = 0
+            for l, p in zip(labels, predictions):
+                if np.abs(l - p) < 0.5:
+                    acc = acc + 1
+            return acc / len(labels)
+
+    elif cost_fn == 'cross_entropy':
         acc = 0
-        for l, p in zip(labels, predictions):
-            if np.abs(l - p) < 1:
+        for l,p in zip(labels, predictions):
+            if p[0] > p[1]:
+                P = 0
+            else:
+                P = 1
+            if P == l:
                 acc = acc + 1
         return acc / len(labels)
 
-    else:
-        acc = 0
-        for l, p in zip(labels, predictions):
-            if np.abs(l - p) < 0.5:
-                acc = acc + 1
-        return acc / len(labels)
 
 def Encoding_to_Embedding(Encoding):
     # Amplitude Embedding / Angle Embedding
@@ -122,12 +135,12 @@ def Encoding_to_Embedding(Encoding):
     return Embedding
 
 
-def Benchmarking(dataset, classes, Unitaries, U_num_params, Encodings, circuit, binary=True):
+def Benchmarking(dataset, classes, Unitaries, U_num_params, Encodings, circuit, cost_fn, binary=True):
     I = len(Unitaries)
     J = len(Encodings)
 
     # save the result in the loss history QCNN and TTN for MNIST.txt file
-    f = open('Result/Result.txt', 'a')
+    f = open('Result/Data/Data_txt/loss history/loss history QCNN and Hierarchical for FASHION.txt', 'a')
 
     for i in range(I):
         for j in range(J):
@@ -141,14 +154,14 @@ def Benchmarking(dataset, classes, Unitaries, U_num_params, Encodings, circuit, 
 
             print("\n")
             print("Loss History for " + circuit + " circuits, " + U + " " + Encoding)
-            loss_history, trained_params = Training.circuit_training(X_train, Y_train, U, U_params, Embedding, circuit)
+            loss_history, trained_params = Training.circuit_training(X_train, Y_train, U, U_params, Embedding, circuit, cost_fn)
 
             if circuit == 'QCNN':
-                predictions = [QCNN_circuit.QCNN(x, trained_params, U, U_params, Embedding) for x in X_test]
+                predictions = [QCNN_circuit.QCNN(x, trained_params, U, U_params, Embedding, cost_fn) for x in X_test]
             elif circuit == 'Hierarchical':
-                predictions = [Hierarchical_circuit.Hierarchical_classifier(x, trained_params, U, U_params, Embedding) for x in X_test]
+                predictions = [Hierarchical_circuit.Hierarchical_classifier(x, trained_params, U, U_params, Embedding, cost_fn) for x in X_test]
 
-            accuracy = accuracy_test(predictions, Y_test, binary)
+            accuracy = accuracy_test(predictions, Y_test, cost_fn, binary)
             print("Accuracy for " + U + " " + Encoding + " :" + str(accuracy))
 
             f.write("Loss History for " + circuit + " circuits, " + U + " " + Encoding)
