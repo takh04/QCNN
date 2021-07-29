9f23e489514266a4728383cce5730fa3f28eaf1a
th817 <73452842+th817@users.noreply.github.com>
2021-07-30 01:14:52 +0900

    add 1D chain structure for SU4

--- Final QOSF_project/QCNN_circuit.py
+++ Final QOSF_project/QCNN_circuit.py
@@ -61,6 +61,22 @@ def QCNN_structure_without_pooling(U, params, U_params):
     conv_layer2(U, param2)
     conv_layer3(U, param3)
 
+def QCNN_1D_circuit(U, params, U_params):
+    param1 = params[0: U_params]
+    param2 = params[U_params: 2*U_params]
+    param3 = params[2*U_params: 3*U_params]
+
+    for i in range(0, 8, 2):
+        U(param1, wires=[i, i + 1])
+    for i in range(1, 7, 2):
+        U(param1, wires=[i, i + 1])
+
+    U(param2, wires=[2,3])
+    U(param2, wires=[4,5])
+
+    U(param3, wires=[3,4])
+
+
 
 dev = qml.device('default.qubit', wires = 8)
 @qml.qnode(dev)
@@ -91,6 +107,8 @@ def QCNN(X, params, U, U_params, embedding_type='Amplitude', cost_fn='cross_entr
         QCNN_structure(unitary.U_SU4, params, U_params)
     elif U == 'U_SU4_no_pooling':
         QCNN_structure_without_pooling(unitary.U_SU4, params, U_params)
+    elif U == 'U_SU4_1D':
+        QCNN_1D_circuit(unitary.U_SU4, params, U_params)
 
     else:
         print("Invalid Unitary Ansatze")
