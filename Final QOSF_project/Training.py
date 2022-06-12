5b377edbc8fe2cc46f3a8690fd184aac23e0355e
Tak Hur <tak@taks-mbp.home>
2022-06-12 13:55:59 +0100

    requires_grad = True for parameters and number of trainable parameters are adjusted for no pooling and 1D layers

--- Final QOSF_project/Training.py
+++ Final QOSF_project/Training.py
@@ -2,14 +2,13 @@
 import QCNN_circuit
 import Hierarchical_circuit
 import pennylane as qml
-import numpy as np
+from pennylane import numpy as np
 import autograd.numpy as anp
 
 def square_loss(labels, predictions):
     loss = 0
     for l, p in zip(labels, predictions):
         loss = loss + (l - p) ** 2
-
     loss = loss / len(labels)
     return loss
 
@@ -18,7 +17,6 @@ def cross_entropy(labels, predictions):
     for l, p in zip(labels, predictions):
         c_entropy = l * (anp.log(p[l])) + (1 - l) * anp.log(1 - p[1 - l])
         loss = loss + c_entropy
-
     return -1 * loss
 
 def cost(params, X, Y, U, U_params, embedding_type, circuit, cost_fn):
@@ -34,35 +32,31 @@ def cost(params, X, Y, U, U_params, embedding_type, circuit, cost_fn):
     return loss
 
 # Circuit training parameters
-
 steps = 200
 learning_rate = 0.01
-
-
 batch_size = 25
-
 def circuit_training(X_train, Y_train, U, U_params, embedding_type, circuit, cost_fn):
     if circuit == 'QCNN':
-        total_params = U_params * 3 + 2 * 3
+        if U == 'U_SU4_no_pooling' or U == 'U_SU4_1D' or U == 'U_9_1D':
+            total_params = U_params * 3
+        else:
+            total_params = U_params * 3 + 2 * 3
     elif circuit == 'Hierarchical':
         total_params = U_params * 7
 
-    params = np.random.randn(total_params)
+    params = np.random.randn(total_params, requires_grad=True)
     opt = qml.NesterovMomentumOptimizer(stepsize=learning_rate)
     loss_history = []
 
     for it in range(steps):
-
         batch_index = np.random.randint(0, len(X_train), (batch_size,))
         X_batch = [X_train[i] for i in batch_index]
         Y_batch = [Y_train[i] for i in batch_index]
-
         params, cost_new = opt.step_and_cost(lambda v: cost(v, X_batch, Y_batch, U, U_params, embedding_type, circuit, cost_fn),
                                                      params)
         loss_history.append(cost_new)
         if it % 10 == 0:
             print("iteration: ", it, " cost: ", cost_new)
-
     return loss_history, params
 
 
