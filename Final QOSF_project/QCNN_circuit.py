3a205116a0916bf314e832d2a967ecb4485b267e
Tak Hur <tak@taks-mbp.home>
2022-06-12 13:54:35 +0100

    Minor changes

--- Final QOSF_project/QCNN_circuit.py
+++ Final QOSF_project/QCNN_circuit.py
@@ -1,10 +1,8 @@
-# Implementation of Quantum Convolutional Neural Network (QCNN) circuit structure.
-
 import pennylane as qml
 import unitary
 import embedding
 
-# Convolutional layers
+# Quantum Circuits for Convolutional layers
 def conv_layer1(U, params):
     U(params, wires=[0, 7])
     for i in range(0, 8, 2):
@@ -19,7 +17,7 @@ def conv_layer2(U, params):
 def conv_layer3(U, params):
     U(params, wires=[0,4])
 
-# Pooling layers
+# Quantum Circuits for Pooling layers
 def pooling_layer1(V, params):
     for i in range(0, 8, 2):
         V(params, wires=[i + 1, i])
@@ -32,22 +30,18 @@ def pooling_layer3(V, params):
 
 
 def QCNN_structure(U, params, U_params):
-
     param1 = params[0:U_params]
     param2 = params[U_params: 2 * U_params]
     param3 = params[2 * U_params: 3 * U_params]
-
     param4 = params[3 * U_params: 3 * U_params + 2]
     param5 = params[3 * U_params + 2: 3 * U_params + 4]
     param6 = params[3 * U_params + 4: 3 * U_params + 6]
 
-
+    # Pooling Ansatz1 is used by default
     conv_layer1(U, param1)
     pooling_layer1(unitary.Pooling_ansatz1, param4)
-
     conv_layer2(U, param2)
     pooling_layer2(unitary.Pooling_ansatz1, param5)
-
     conv_layer3(U, param3)
     pooling_layer3(unitary.Pooling_ansatz1, param6)
 
@@ -73,7 +67,6 @@ def QCNN_1D_circuit(U, params, U_params):
 
     U(param2, wires=[2,3])
     U(param2, wires=[4,5])
-
     U(param3, wires=[3,4])
 
 
@@ -111,7 +104,6 @@ def QCNN(X, params, U, U_params, embedding_type='Amplitude', cost_fn='cross_entr
         QCNN_1D_circuit(unitary.U_SU4, params, U_params)
     elif U == 'U_9_1D':
         QCNN_1D_circuit(unitary.U_9, params, U_params)
-
     else:
         print("Invalid Unitary Ansatze")
         return False
